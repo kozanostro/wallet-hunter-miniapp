8b874b99b5529770a5cac8404ba8817722d97aca
kozanostro <kozanostro41@gmail.com>
2025-12-27 03:22:08 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+// app.js
+
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
+
+// Получаем данные пользователя Telegram
+const user = tg.initDataUnsafe?.user;
+
+if (!user) {
+    document.getElementById('app').innerHTML =
+        '<p>Ошибка инициализации Telegram</p>';
+} else {
+    // сохраняем пользователя
+    localStorage.setItem('tg_user', JSON.stringify({
+        id: user.id,
+        username: user.username || '',
+        first_name: user.first_name || '',
+        last_name: user.last_name || ''
+    }));
+
+    // редирект на регистрацию
+    window.location.href = 'pages/register.html';
+}
 
